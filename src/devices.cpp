fc401ef6e9da27c230c91067636a2a668457970b
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-02-26 20:06:25 -0300

    get_devices() and get_home_dir() created

--- src/devices.cpp
+++ src/devices.cpp
@@ -10,6 +10,9 @@
 #include <string>
 #include <fstream>
 #include <iostream> //test
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 #include "devices.h"
 
 using std::vector;
@@ -20,27 +23,35 @@ using std::getline;
 namespace trashy { namespace config {
 
 	void Devices::get_devices() {
+		usrhome = get_home_dir();
 		system("mkdir -p ~/.trashy"); // creates program folder
 		system("rm -f ~/.trashy/devlist");
 		system("rm -f ~/.trashy/remlist");
 		system("mount -l | grep -i /dev/s | grep -v tmpfs | cat >~/.trashy/devlist"); 
 		system("mount -l | grep -i // | cat >~/.trashy/remlist");
 
-		ifstream fileInput(DEV_LIST);
+		ifstream fileInput(usrhome+DEV_LIST);
 
-		std::cout << "Before read file\n";
 		if(fileInput) {
 			string line;
 
-			std::cout << "Read file\n";
 			while(getline(fileInput, line)) {
-				std::cout << line << std::endl;
+				// std::cout << line << std::endl; // test line
 			}
 
 			fileInput.close();
+		}
+	}
 
+	string Devices::get_home_dir() {
+		const char *homedir;
+
+		if ((homedir = getenv("HOME")) == NULL) {
+			struct passwd *pw = getpwuid(getuid());
+		    homedir = pw->pw_dir;
 		}
 
+		return string(homedir);
 	}
 
 }} // trashy::config
--- src/devices.h
+++ src/devices.h
@@ -13,11 +13,13 @@ using std::vector;
 using std::string;
 
 namespace trashy { namespace config {
-	const static string REM_LIST = "~/.trashy/remlist";
-	const static string DEV_LIST = "~/.trashy/devlist";
 
 	class Devices {
-	vector<string> dev_roots; // devices root paths
+		vector<string> dev_roots; // devices root paths
+		const string REM_LIST = "/.trashy/remlist";
+		const string DEV_LIST = "/.trashy/devlist";
+		string usrhome;
+
 
 	public:
 		Devices() {
@@ -26,6 +28,7 @@ namespace trashy { namespace config {
 
 	private:
 		void get_devices();
+		string get_home_dir();
 	};
 
 
